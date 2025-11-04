5b18f9bc49a2a7d9225cdb1c152b38931e92e00d
AndreLuis933 <andreluissouza933@gmail.com>
2025-11-04 15:43:58 -0400

    .

--- front/src/consts/const.tsx
+++ front/src/consts/const.tsx
@@ -2,3 +2,9 @@ export const TYPE_MAP: Record<string, "animes" | "filmes"> = {
   anime: "animes",
   movie: "filmes",
 };
+
+  // "tougen-anki",
+  // "one-piece",
+  // "spy-x-family-3",
+  // "boku-no-hero-academia-final",
+  // "gachiakuta",
