0b28edceadfc8ce2083fb46e4f45d2e77ff170ce
AndreLuis933 <andreluissouza933@gmail.com>
2025-11-06 20:47:50 -0400

    .

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -3,11 +3,15 @@ import Home from "./pages/Home";
 import Base from "./pages/Base";
 import AnimePage from "./pages/Anime/AnimePage";
 import PlayerPage from "./pages/Player/PlayerPage";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 const App = () => {
   return (
     <>
       <BrowserRouter>
+        <Analytics />
+        <SpeedInsights />
         <Routes>
           <Route path="/" element={<Base />}>
             <Route index element={<Home />} />
